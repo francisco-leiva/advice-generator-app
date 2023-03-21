266f119607dd7508d3f532fd61f0c6d9b962e1c7
francisco-leiva <leivafrancisco631@gmail.com>
2023-03-21 00:54:07 -0300

    Initial state

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <section>
+        <span>Advice #{/*Advice ID goes here */}</span>
+
+        <p>{/* Advice text goes here */}</p>
+      </section>
+
+      <div class='attribution'>
+        Challenge by{' '}
+        <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+          Frontend Mentor
+        </a>
+        . Coded by <a href='#'>Your Name Here</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
